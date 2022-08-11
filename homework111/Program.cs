1dc3e175a6ed89711f163ecb607e640dc552cdb5
Farid0558475484 <faridmh@code.edu.az>
2022-08-11 18:52:29 +0400

    inital commit

---
+++ homework111/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homework111
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            User Usertype = new User("wertydfghj");
+            Usertype.Password = "Azerbaycan";
+            Usertype.ShowProfile();
+        }
+    }
+}
---
+++ homework111/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace homework111
+{
+    public class User
+    {
+        string _password;
+        string _userName;
+        
+    
+    public string UserName {
+        get
+        {
+            return (_userName);
+
+
+        }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    UserName = value;
+
+
+                }
+            
+            }
+
+
+    }
+}
+    public string Password
+    {
+        get
+        {
+            return (_password);
+
+        }
+        set
+        {
+            if(value.Length>= 8 && value.Length<=25 && HasDigit(value) && HasUpper (value) && Haslower(value))
+            {
+                Password = value;
+            }
+
+
+        }
+    
